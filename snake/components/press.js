bdf98dbf077aae43d42d6dd9dec4d393e8d88b78
Andrew Liu <andrew.a.liu.19@dartmouth.edu>
2018-09-20 11:37:38 -0400

    added stat tracking, images, tweaked formatting

--- snake/components/press.js
+++ snake/components/press.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet, View, Text, Image, TouchableOpacity, FlatList,
+  StyleSheet, View, Text, Image, TouchableOpacity, FlatList, ImageBackground,
 } from 'react-native';
 
 class Press extends Component {
@@ -159,12 +159,28 @@ class Press extends Component {
     //   this.setState({ reset_state: false });
     // }
 
-    const { snek_counter, snek_rewards } = this.state;
+    const {
+      snek_counter,
+      snek_rewards,
+      caseNum,
+      chimpNum,
+      sabotageNum,
+      ibvetNum,
+      securitizeNum,
+      djsolNum,
+      insidertradeNum,
+      makemarketNum,
+      mbsNum,
+      mercerNum,
+    } = this.state;
     if (snek_counter > 9 && snek_rewards.length < 1) {
       const interview = {
         id: 1,
         snekPoints: 10,
         title: 'Get Case Interview Secrets',
+        countLabel: 'Books Purchased: ',
+        rate: 1,
+        background: 'https://images.gr-assets.com/books/1340744133l/15056959.jpg',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(interview) }));
       // this.setState({ reset_state: true });
@@ -175,6 +191,9 @@ class Press extends Component {
         id: 2,
         snekPoints: 20,
         title: 'WSO Senior Chimp',
+        countLabel: 'Senior Chimps: ',
+        rate: 2,
+        background: 'https://static1.squarespace.com/static/53adf1a4e4b07a4f011714d8/57c10cb92994ca2851619b9f/57eff43d725e25e9149ec2e4/1475343421903/Wall+Street+Oasis.jpg?format=1000w',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(chimp) }));
       // this.setState({ reset_state: true });
@@ -185,6 +204,9 @@ class Press extends Component {
         id: 3,
         snekPoints: 500,
         title: 'Sabotage Interview of Friend',
+        countLabel: 'Friends Sabotaged: ',
+        rate: 50,
+        background: 'https://image.shutterstock.com/image-vector/rejected-red-square-grunge-stamp-260nw-335102924.jpg',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(sabotage) }));
     }
@@ -194,6 +216,9 @@ class Press extends Component {
         id: 4,
         snekPoints: 10000,
         title: '2y IB Vet',
+        countLabel: 'IB Vets: ',
+        rate: 500,
+        background: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Goldman_Sachs.svg/200px-Goldman_Sachs.svg.png',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(ibvet) }));
     }
@@ -203,6 +228,9 @@ class Press extends Component {
         id: 5,
         snekPoints: 100000,
         title: 'Securitize a Snek Farm',
+        countLabel: 'Snek Farms: ',
+        rate: 5000,
+        background: 'https://www.businessdayonline.com/wp-content/uploads/2016/12/snake_farm.jpg',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(securitize) }));
     }
@@ -212,6 +240,9 @@ class Press extends Component {
         id: 6,
         snekPoints: 500000,
         title: 'Party with DJ Sol',
+        countLabel: 'Parties: ',
+        rate: 10000,
+        background: 'https://nyppagesix.files.wordpress.com/2018/07/dj-goldman-saks.jpg?quality=90&strip=all&w=618&h=410&crop=1',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(djsol) }));
     }
@@ -221,6 +252,9 @@ class Press extends Component {
         id: 7,
         snekPoints: 1000000,
         title: 'Insider Trade with Stevey',
+        countLabel: 'Insider Trades: ',
+        rate: 25000,
+        background: 'https://image.shutterstock.com/image-vector/vector-illustration-cartoon-thief-260nw-130235285.jpg',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(insidertrade) }));
     }
@@ -230,6 +264,9 @@ class Press extends Component {
         id: 8,
         snekPoints: 12500000,
         title: 'Make Markets with Ken',
+        countLabel: 'Markets Made: ',
+        rate: 100000,
+        background: 'https://s3.amazonaws.com/lowres.cartoonstock.com/food-drink-biblical-feeding_of_the_5000-bread-fish-feed_the_5000-jlo0174_low.jpg',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(makemarket) }));
     }
@@ -238,7 +275,10 @@ class Press extends Component {
       const mbs = {
         id: 9,
         snekPoints: 50000000,
-        title: 'Crash Economy with Mortgage Backed Securities',
+        title: 'Crash Economy with MBSs',
+        countLabel: 'Economies Crashed: ',
+        rate: 1000000,
+        background: 'http://www.internationalinvestment.net/wp-content/uploads/2016/04/MBS-image-740x360.jpg',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(mbs) }));
     }
@@ -249,9 +289,15 @@ class Press extends Component {
         id: 10,
         snekPoints: 10000000000,
         title: 'Rig Presidency with Bob Mercer',
+        countLabel: 'Presidencies Rigged: ',
+        rate: 100000000,
+        background: 'https://betanews.com/wp-content/uploads/2017/06/cartoon-trump-flag-600x375.jpg',
       };
       this.setState(prevState => ({ snek_rewards: prevState.snek_rewards.concat(mercer) }));
     }
+
+    const currentSnekRate = caseNum + 2 * chimpNum + 50 * sabotageNum + 500 * ibvetNum + 5000 * securitizeNum + 10000 * djsolNum + 25000 * insidertradeNum + 100000 * makemarketNum + 1000000 * mbsNum + 100000000 * mercerNum;
+    const itemNumArray = [caseNum, chimpNum, sabotageNum, ibvetNum, securitizeNum, djsolNum, insidertradeNum, makemarketNum, mbsNum, mercerNum];
 
     return (
 
@@ -270,13 +316,41 @@ class Press extends Component {
           Snekiness:
             {' '}
             {snek_counter}
+            {'\nSnek Rate: '}
+            {currentSnekRate}
+            {currentSnekRate === 1 ? ' snek per second' : ' sneks per second'}
           </Text>
         </View>
 
         <View style={styles.flatlist}>
           <FlatList
             data={snek_rewards}
-            renderItem={({ item }) => <TouchableOpacity onPress={() => this._onPressRewards(item)}><Text style={styles.item}>{item.title}</Text></TouchableOpacity>}
+            style={styles.listcontainer}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => this._onPressRewards(item)}>
+                <ImageBackground source={{ uri: item.background }}
+                  style={{
+                    width: '100%', height: '100%', flex: 1,
+                  }}
+                  resizeMode="repeat"
+                >
+                  <View style={styles.listcontainer}>
+                    <Text style={styles.item}>{item.title}</Text>
+                    <View style={styles.statscontainer}>
+                      <Text style={styles.statsitem}>
+                        {'Sneks: '}
+                        {item.snekPoints}
+                      </Text>
+                      <Text style={styles.statsitem}>
+                        {'Rate (sneks/s): '}
+                        {item.rate}
+                      </Text>
+                      <Text style={styles.statsitem}>{item.countLabel + itemNumArray[item.id - 1]}</Text>
+                    </View>
+                  </View>
+                </ImageBackground>
+              </TouchableOpacity>
+            )}
           />
         </View>
       </View>
@@ -296,6 +370,11 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
 
+  listcontainer: {
+    flexDirection: 'column',
+    backgroundColor: 'rgba(255,255,255,.88)',
+  },
+
   image: {
     width: 400,
     height: 300,
@@ -306,6 +385,7 @@ const styles = StyleSheet.create({
     marginLeft: 0,
     paddingRight: 0,
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: 'red',
   },
 
@@ -313,6 +393,19 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 18,
     height: 44,
+    flex: 1,
+    flexWrap: 'wrap',
+    fontWeight: 'bold',
+  },
+
+  statscontainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+
+  statsitem: {
+    fontSize: 12,
+    paddingLeft: 10,
   },
 });
 
